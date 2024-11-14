636d8ed7fe5aab73f27579ba47c988013621dc59
jmws00 <jmws@op.pl>
2024-11-14 23:25:12 +0100

    Update test1.c

--- test1.c
+++ test1.c
@@ -21,17 +21,12 @@ int main() {
     // Set direction: output on channel 1 (for controlling `time`)
     XGpio_SetDataDirection(&Gpio, TIME_OUTPUT_CHANNEL, 0x0);
 
-    // Set the time value (example value)
-    time_value = 100; // Set the time value according to project requirements
+    // Set the time value
+    time_value = 100;
 
     // Send the value to GPIO
     xil_printf("Sending value %u to the pulse_generator module\n\r", time_value);
     XGpio_DiscreteWrite(&Gpio, TIME_OUTPUT_CHANNEL, time_value);
-/*
-    while (1) {
-        // The program can monitor output, add handling, etc.
-        // Optionally, code for reading or additional logic can be added here
-    }
-*/
+
     return 0;
 }
