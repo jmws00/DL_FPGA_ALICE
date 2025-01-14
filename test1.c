8296045924ac6156d78a9c9a358319fd40d83679
jmws00 <jmws@op.pl>
2025-01-14 20:29:54 +0100

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+
+#include "xil_io.h"
 #include "xparameters.h"
 #include "xgpio.h"
+#include "xil_types.h"
+#include "sleep.h"
+#include "counter_signal_generator_axi.h"
 
-#define GPIO_DEVICE_ID XPAR_AXI_GPIO_0_DEVICE_ID // ID for GPIO setting `time`
-#define TIME_OUTPUT_CHANNEL 1                   // Channel for `time`
+#define GPIO_DEVICE_ID_LED XPAR_AXI_GPIO_0_DEVICE_ID
+#define LED_OUTPUT_CHANNEL 1
 
-XGpio Gpio;
+int generate(u32 N, u32 T1,  u32 *signal_out);
 
-int main() {
-    int Status;
-    u32 time_value = 0;
+int main()
+{
+    init_platform();
+	XGpio GpioLED;
+    XGpio_Initialize(&GpioLED, GPIO_DEVICE_ID_LED);
 
-    // Initialize GPIO
-    Status = XGpio_Initialize(&Gpio, GPIO_DEVICE_ID);
-    if (Status != XST_SUCCESS) {
-        xil_printf("GPIO initialization failed\n");
-        return XST_FAILURE;
+    u32 N = 0b11111111; // Przykładowy wzór (8-bitowa liczba binarna)
+    u32 T1 = 500;     // 32-bitowa liczba dziesiętna;
+    u32 signal_out;
+
+    while(1)
+    {
+    	generate(N, T1, &signal_out);
+    	usleep(250000);
+    	xil_printf("Signal out: %x\n\r", signal_out);
     }
 
-    // Set direction: output on channel 1 (for controlling `time`)
-    XGpio_SetDataDirection(&Gpio, TIME_OUTPUT_CHANNEL, 0x0);
 
-    // Set the time value
-    time_value = 100;
-
-    // Send the value to GPIO
-    xil_printf("Sending value %u to the pulse_generator module\n\r", time_value);
-    XGpio_DiscreteWrite(&Gpio, TIME_OUTPUT_CHANNEL, time_value);
-
+    cleanup_platform();
     return 0;
 }
+
+
